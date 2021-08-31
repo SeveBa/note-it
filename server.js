33ff670e1f13f35e6496e3cf4e3ab76a49f37124
Seve Barrutia <sevebarrutia@me.com>
2021-08-30 23:41:59 -0500

    configure server & add readme

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
+
+// require('./routes/apiRoutes')
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`)
+})
